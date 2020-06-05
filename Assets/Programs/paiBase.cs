a6021420726c1af0428e939d24791513f49514cb
Melony5 <mk41kisa@outlook.jp>
2020-06-05 17:31:11 +0900

    paiBaseクラスの実装

---
+++ Assets/Programs/paiBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paiBase : MonoBehaviour
+{
+    private int type;
+    private int number;
+
+    public int getType(int type) {
+        return type;
+    }
+
+    public int getNumber(int number) {
+        return number;
+    }
+}
