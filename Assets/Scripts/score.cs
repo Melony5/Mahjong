4325e28d0064728dad6bcf30ad8af6072635d412
メジロ <meziro0415.dai@gmail.com>
2020-06-16 02:18:07 +0900

    シャン点数の枠組み

--- Assets/Scripts/score.cs
+++ Assets/Scripts/score.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class score : MonoBehaviour
 {
-    
+    //クラスの中に入れるべきなのかどうかもわからない.ファイルすら分けるべきかも
+    //向聴数(あがりやリーチまでの数)
+    int checkSyanTen() {//intにするかどうかも決まってない,引数も決まってない
+        //国士無双の場合
+        //七対子の場合
+        //一般の場合
+    }
 }
