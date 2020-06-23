9e6520bcbfa8c9604d57dc81a557e86c8395de7a
メジロ <meziro0415.dai@gmail.com>
2020-06-24 01:01:19 +0900

    特に変更なし

--- Assets/Scripts/score.cs
+++ Assets/Scripts/score.cs
@@ -8,23 +8,25 @@ public class score : MonoBehaviour
     //向聴数(あがりやリーチまでの数)
     //国士無双の場合
     int SyantenKokusiMusou() {//intにするかどうかも決まってない,引数も決まってない
-        int syanten;
+        int syanten = 13;
+        bool toitu = false;
+       
         for (int i = 0; i < 13; i++) {
             //手牌の種類(一九字牌)を数える
-            syanten++;
+            syanten--;
         }
         return syanten;//向聴数を返す
     }
         //七対子の場合
 
     int SyantenTitoi() {
-        int syanten;
+        int syanten = 0;
+        int toitu = 0;
         for(int i = 0; i < 13; i++) {
             //七対子の向聴数=6-対子の数(例外処理もある)
-            syanten++;
+            toitu++;
         }
         return syanten;
     }
         //一般の場合
     }
-}
