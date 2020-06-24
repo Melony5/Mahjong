2e878cdbacf46216c1ca48241836f25322fc0575
メジロ <meziro0415.dai@gmail.com>
2020-06-24 17:35:25 +0900

    対子をチェックする関数を作成

--- Assets/Scripts/score.cs
+++ Assets/Scripts/score.cs
@@ -7,12 +7,21 @@ public class score : MonoBehaviour
     //クラスの中に入れるべきなのかどうかもわからない.ファイルすら分けるべきかも
     //向聴数(あがりやリーチまでの数)
     //国士無双の場合
-    int SyantenKokusiMusou() {//intにするかどうかも決まってない,引数も決まってない
+
+    //任意の手牌とその次とが対子どうかを調べて真偽を返す
+    bool CheckToitu(int[] syuhai,int i) {
+        if (syuhai[i] == syuhai[i + 1]) return true;
+        else return false;
+    }
+        
+    int SyantenKokusiMusou(int[]syuhai) {//intにするかどうかも決まってない,引数も決まってない
         int syanten = 13;
         bool toitu = false;
        
         for (int i = 0; i < 13; i++) {
             //手牌の種類(一九字牌)を数える
+            toitu = CheckToitu(syuhai, i);
+            if (CheckToitu(syuhai, i) == true /*"かつ19字牌だったら"*/) syanten--;
             syanten--;
         }
         return syanten;//向聴数を返す
