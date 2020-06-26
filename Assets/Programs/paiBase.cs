44bd2a18725588c2229abadd554f0d5fd4644b02
Melony5 <mk41kisa@outlook.jp>
2020-06-26 16:54:20 +0900

    少し考え直し

--- Assets/Programs/paiBase.cs
+++ Assets/Programs/paiBase.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class paiBase : MonoBehaviour
 {
-    private enum kind{ manzu, sozu, pinzu, sangen, fon};
-    private enum num {ii = 1, ryan, san, su, uu, ro, chi, pa,
+    public enum kind{ manzu, sozu, pinzu, sangen, fon};
+    public enum num {ii = 1, ryan, san, su, uu, ro, chi, pa,
                       kyu, haku, hatsu, chun, ton, nan, sya, pe};
 
     GameObject[,] pai = new GameObject[5, 16];
 
-
-    public GameObject getType(int type) {
-        return pai[0, 0];
-    }
 }
