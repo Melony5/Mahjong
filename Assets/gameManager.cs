87df04bb9614e551e8710c14aef34f2969124d0e
Melony5 <mk41kisa@outlook.jp>
2020-06-26 17:31:08 +0900

    ゲーム進行の基盤を定義

---
+++ Assets/gameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game : MonoBehaviour
+{
+    public int playerTurn;
+    player play;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTurn = 1;
+        play = new player();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        play.move(playerTurn);
+        
+        if(playerTurn == 4) {
+            playerTurn = 1;
+        } else {
+            playerTurn++;
+        }
+    }
+}
