4d898c322947705d7bcd5c2742beadf2d295c0a3
Melony5 <mk41kisa@outlook.jp>
2020-06-29 18:05:25 +0900

    playerクラスの流れを大体作った scoreとの兼ね合いを考慮した

--- Assets/gameManager.cs
+++ Assets/gameManager.cs
@@ -6,17 +6,26 @@ public class game : MonoBehaviour
 {
     public int playerTurn;
     player play;
+
+    bool agari;
     // Start is called before the first frame update
     void Start()
     {
-        playerTurn = 1;
         play = new player();
+        playerTurn = 1;
+
+        agari = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        play.move(playerTurn);
+        agari = play.move(playerTurn);
+
+        if(agari == true) {
+            // スコア計算
+            // 結果表示画面
+        }
         
         if(playerTurn == 4) {
             playerTurn = 1;
--- Assets/player.cs
+++ Assets/player.cs
@@ -4,10 +4,46 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    public void move(int playerTurn) {
-        syuhai(playerTurn); 
+    paiBase pai;
+    board board;
+    score judge;
+
+    int syanten;
+    bool reach;
+    bool agari;
+
+    player() {
+        pai = new paiBase();
+        board = new board();
+        judge = new score();
+        reach = false;
+        agari = false;
     }
-    public void syuhai() {
-        
+
+    public bool move(int playerTurn) {
+        tehai(playerTurn);
+        if(reach == true) { // 立直の時の処理
+            // 山から牌をとってきて、scoreクラスに渡す
+        } else {
+            score(playerTurn);
+        }
+
+        return agari;
+    }
+
+    // プレイヤーに取捨選択させてgetsetする。
+    public void tehai(playerTurn) {
+        // board.山のリスト型 これで山参照
+        // 捨てる牌をクリっくさせる。
+        // board.kawaにsetする。
+    }
+
+    public void score(playerTurn) {
+        // scoreクラスをインスタンス化してやってもらう
+        syanten = judge.agari();
+
+        if(syanten == 0) {
+            reach = true;
+        }
     }
 }
